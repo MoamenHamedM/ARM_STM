0b585298aac1be5b9b8d72a385f6e9ce0755be9c
MoamenHamedM <moamen.h.abdellatef@gmail.com>
2024-03-31 15:03:11 +0200

    adding DMA driver documentation

--- inc/MCAL/DMA_DRIVER.h
+++ inc/MCAL/DMA_DRIVER.h
@@ -151,56 +151,342 @@
 /************************************************APIs****************************************************/
 /********************************************************************************************************/
 
+/*
+ * Enable or disable a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_StreamState -> State of the DMA stream (DMA_STREMSTATE_DISABLE or DMA_STREMSTATE_ENABLE).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_CTRL_StreamEnable_Disable(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_StreamState);
 
+/*
+ * Get the interrupt status of a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   DMA_Strm -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   Status -> Pointer to store the interrupt status.
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_GET_InterruptStatus(void *DMA_Peri, u64_t DMA_Strm, u32_t *Status);
 
+/*
+ * Clear the interrupt flag of a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   DMA_Strm -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_CTRL_ClearInterruptFlag(void *DMA_Peri, u64_t DMA_Strm);
 
+/*
+ * Enable or disable interrupts for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   DMA_Peri_F -> Interrupt flag (DMA_TCIE_FLAG, DMA_HTIE_FLAG, DMA_TEIE_FLAG, DMA_DMEIE_FLAG, or DMA_FIFO_EI_FLAG).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   State -> State of the interrupt (DMA_INT_STATE_ENABLE or DMA_INT_STATE_DISABLE).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_CTRL_InterruptEnable_Disable(void *DMA_Peri, u64_t DMA_Peri_F, u8_t STRM_Num, u8_t State);
 
+/*
+ * Select a DMA channel for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_Chn -> DMA channel (DMA_CHN_0 to DMA_CHN_7).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_SEL_Channel(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_Chn);
 
+/*
+ * Configure memory burst mode for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_Mem_B -> Memory burst mode (DMA_MEM_B_SINGLE, DMA_MEM_B_INCR4, DMA_MEM_B_INCR8, or DMA_MEM_B_INCR16).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_CTRL_MemoryBurst(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_Mem_B);
 
+/*
+ * Configure peripheral burst mode for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_Peri_B -> Peripheral burst mode (DMA_PERI_B_SINGLE, DMA_PERI_B_INCR4, DMA_PERI_B_INCR8, or DMA_PERI_B_INCR16).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_CFG_PeripheralBurst(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_Peri_B);
 
+/*
+ * Select the current target (memory 0 or memory 1) for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_CT -> Current target (DMA_CT_MEM0 or DMA_CT_MEM1).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_SEL_CurrentTarget(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_CT);
 
+/*
+ * Get the current target for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_CT -> Pointer to store the current target.
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_GET_CurrentTarget(void *DMA_Peri, u8_t STRM_Num, u32_t *DMA_CT);
 
+/*
+ * Configure double buffer mode for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_DB -> Double buffer mode (DMA_DB_OFF or DMA_DB_ON).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_CFG_DoubleBuffer(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_DB);
 
+/*
+ * Configure priority level for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_Pri -> Priority level (DMA_PRIORITY_LOW, DMA_PRIORITY_MED, DMA_PRIORITY_HIGH, or DMA_PRIORITY_VHIGH).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_CFG_PriorityLevel(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_Pri);
 
+/*
+ * Configure peripheral increment and offset size for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_Pincos -> Peripheral increment and offset size (DMA_PINCOS_PSIZE or DMA_PINCOS_FIXED).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_CFG_PeriIncr_OffsetSize(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_Pincos);
 
+/*
+ * Configure memory data size for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_Mem_S -> Memory data size (DMA_MEM_SIZE_BYTE, DMA_MEM_SIZE_HWORD, or DMA_MEM_SIZE_WORD).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_CFG_MemoryDataSize(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_Mem_S);
 
+/*
+ * Configure peripheral data size for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_Peri_S -> Peripheral data size (DMA_PERI_SIZE_BYTE, DMA_PERI_SIZE_HWORD, or DMA_PERI_SIZE_WORD).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_CFG_PeripheralDataSize(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_Peri_S);
 
+/*
+ * Configure memory increment mode for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_Mem_Incr -> Memory increment mode (DMA_MEM_INCR_OFF or DMA_MEM_INCR_ON).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_CFG_MemoryIncrement(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_Mem_Incr);
 
+/*
+ * Configure peripheral increment mode for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_Peri_Incr -> Peripheral increment mode (DMA_PERI_INCR_OFF or DMA_PERI_INCR_ON).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_CFG_PeripheralIncrement(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_Peri_Incr);
 
+/*
+ * Configure circular mode for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_Circ -> Circular mode state (DMA_CIRC_OFF or DMA_CIRC_ON).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_CFG_CircularMode(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_Circ);
 
-Error_Status DMA_CFG_PeripherlaFlowCTRL(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_Peri_F);
+/*
+ * Configure peripheral flow control for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_Peri_F -> Peripheral flow control (DMA_PERI_FLOW_DMA or DMA_PERI_FLOW_PERI).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
+Error_Status DMA_CFG_PeripheralFlowCTRL(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_Peri_F);
 
+/*
+ * Set the data transfer direction for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   Data_Dir -> Data transfer direction (DMA_TRANS_DIR_P_TO_M, DMA_TRANS_DIR_M_TO_P, or DMA_TRANS_DIR_M_TO_M).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_SET_DataTransDirection(void *DMA_Peri, u8_t STRM_Num, u32_t Data_Dir);
 
+/*
+ * Set the number of data items to be transferred for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   Num -> Number of data items to be transferred.
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_SET_NumOfData(void *DMA_Peri, u8_t STRM_Num, u16_t Num);
 
+/*
+ * Set the peripheral address for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   Peri_Address -> Peripheral address.
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_SET_PeripheralAddress(void *DMA_Peri, u8_t STRM_Num, u32_t Peri_Address);
 
+/*
+ * Set the memory address for a DMA stream (Memory 0).
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   Mem_Address -> Memory address (Memory 0).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_SET_Memory0_Address(void *DMA_Peri, u8_t STRM_Num, u32_t Mem_Address);
 
+/*
+ * Set the memory address for a DMA stream (Memory 1).
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   Mem_Address -> Memory address (Memory 1).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_SET_Memory1_Address(void *DMA_Peri, u8_t STRM_Num, u32_t Mem_Address);
 
+/*
+ * Get the FIFO status for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   Status -> Pointer to store the FIFO status.
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_GET_FIFO_Status(void *DMA_Peri, u8_t STRM_Num, u8_t *Status);
 
+/*
+ * Configure direct mode for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_Dir_M -> Direct mode configuration (DMA_DIR_MODE_ENABLE or DMA_DIR_MODE_DISABLE).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_CTRL_DirectMode(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_Dir_M);
 
+/*
+ * Configure the FIFO threshold for a DMA stream.
+ *
+ * Parameters:
+ *   DMA_Peri -> Pointer to the DMA peripheral base address (DMA_1 or DMA_2).
+ *   STRM_Num -> DMA stream number (DMA_STRM_0 to DMA_STRM_7).
+ *   DMA_Fifo_Thres -> FIFO threshold (DMA_FIFO_THRES_1_4, DMA_FIFO_THRES_1_2, DMA_FIFO_THRES_3_4, or DMA_FIFO_THRES_FULL).
+ *
+ * Return:
+ *   Status_NOK, Status_OK, Status_Null_Pointer, Status_Invalid_Input
+ */
 Error_Status DMA_CFG_FIFOThreshold(void *DMA_Peri, u8_t STRM_Num, u64_t DMA_Fifo_Thres);
 
 #endif
